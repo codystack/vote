39f3c4b5b80c796374941eda4f2664ee79a5079e
farintin <farintinobialor@gmail.com>
2020-02-25 21:49:16 +0100

    worked on free or charged mode votting settings

--- components/wavebg.php
+++ components/wavebg.php
@@ -101,7 +101,7 @@ function applyVote ($conn, $scores, $pseudo, $visitor_ip, $contestant_id) {
             </div>
             </div>
             <div>
-               <!--<svg class=\"waves\" xmlns=\"http://www.w3.org/2000/svg\" xmlns:xlink=\"http://www.w3.org/1999/xlink\" viewBox=\"0 24 150 28\" preserveAspectRatio=\"none\" shape-rendering=\"auto\">
+               <svg class=\"waves\" xmlns=\"http://www.w3.org/2000/svg\" xmlns:xlink=\"http://www.w3.org/1999/xlink\" viewBox=\"0 24 150 28\" preserveAspectRatio=\"none\" shape-rendering=\"auto\">
                     <defs>
                         <path id=\"gentle-wave\" d=\"M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z\" />
                     </defs>
@@ -112,7 +112,7 @@ function applyVote ($conn, $scores, $pseudo, $visitor_ip, $contestant_id) {
                         <use xlink:href=\"#gentle-wave\" x=\"48\" y=\"7\" fill=\"#fff\" />
                     </g>
                 </svg>
-            </div>-->
+            </div>
             ";
             exit();
         }
@@ -132,7 +132,7 @@ function applyVote ($conn, $scores, $pseudo, $visitor_ip, $contestant_id) {
         echo "</div>";
     }
    ?>
-    <!--Waves Container
+    <!--Waves Container-->
     <div>
         <svg class="waves" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 24 150 28" preserveAspectRatio="none" shape-rendering="auto">
             <defs>
